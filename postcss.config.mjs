ea24841c29f6de33787c8bd6fb275acbadf9503c
Pushkar Kumar <pushkar.kumar@rapidshyp.com>
2025-04-25 12:17:37 +0530

    applied radix ui and tailwindcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+export default {
+  plugins: {
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
-export default config;
+
+
